fd033b431cab5259f3a87cd101891fc3ec570d2c
CJ Jessett <email": null>
2016-08-24 19:44:14 -0500

    mess with associations

--- wiki-app/app/models/user.rb
+++ wiki-app/app/models/user.rb
@@ -1,12 +1,10 @@
-class User < ApplicationRecord
-
+class User < ActiveRecord::Base
   has_secure_password
 
-  has_and_belongs_to_many :groups
   has_many :articles
+  has_many :groups, :through => :groups_users
 
   def fullname
     self.first_name.capitalize + " " + self.last_name.capitalize
   end
-
 end
